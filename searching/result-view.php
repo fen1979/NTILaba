ff131cd4aac491743fd973939fbde874c80a690a
fen1979 <a1m9i7r9@gmail.com>
2024-08-12 09:51:10 +0300

    added filtering and table header creation function, changed some details fixed some issues

--- searching/result-view.php
+++ searching/result-view.php
@@ -140,6 +140,7 @@ function viewSupplier($result, $request, $mySearchString): void
  */
 function viewLineProject($result, $col)
 {
+    //i ВЫВОД РЕЗУЛЬТАТОВ ПОИСКА В МОДАЛЬНОМ ОКНЕ
     if ($result) { ?>
         <thead>
         <tr>
@@ -167,7 +168,7 @@ function viewLineProject($result, $col)
         <?php } ?>
         </tbody>
         <?php
-    }else{
+    } else {
         echo 'EMPTY';
     }
 }
@@ -181,74 +182,114 @@ function viewLineProject($result, $col)
  */
 function viewFullProject($result, $user)
 {
-    foreach ($result as $value) {
-        if ($value['archivation']) {
-            $projectId = $value['id'];
-            // получаем фото/много фоток
-            $imgPath = getProjectFrontPicture($projectId, $user['preview'], true);
-            $projectName = str_replace('_', ' ', $value['projectname']);
-            $shareLink = SHARE_LINK_ROUTE . $value['sharelink'];
-            $customerName = $value['customername'];
-            $descInfo = $value['extra'];
-            $startDate = $value['date_in'];
-            $revision = $value['revision']; ?>
-            <div class="col-md-4">
+    if ($_SESSION['preview_mode']) {
+        //i ВЫВОД ТАБЛИЧНОГО ВИДА ПРОЕКТА ПРИ ПОИСКЕ
+        $settings = getUserSettings($user, PROJECTS);
+        ?>
+        <table class="p-3" id="project-table">
+            <!-- header -->
+            <thead>
+            <tr style="white-space: nowrap">
+                <?= CreateTableHeaderUsingUserSettings($settings, 'project-table', PROJECTS, '<th>Share Project</th>') ?>
+            </tr>
+            </thead>
+            <!-- table -->
+            <tbody>
+            <?php
+            foreach ($result as $value) {
+                $shareLink = SHARE_LINK_ROUTE . $value['sharelink'];
+                $projectId = $value['id'];
+                ?>
+                <tr class="item-list" data-id="<?= $projectId; ?>">
+                    <?php
+                    if ($settings) {
+                        foreach ($settings as $item => $_) {
+                            echo '<td>' . $value[$item] . '</td>';
+                        }
+                    }
+                    ?>
+                    <td>
+                        <button type="button" class=" w-100 btn btn-sm btn-outline-diliny share-project" data-share-link="<?= $shareLink; ?>">
+                            <i class="bi bi-share-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    <?php } else {
+        //i ВЫВОД ПОЛНОГО ВИДА ПРОЕКТА ПРИ ПОИСКЕ
+        foreach ($result as $value) {
+            if ($value['archivation']) {
+                $projectId = $value['id'];
+                // получаем фото/много фоток
+                $imgPath = getProjectFrontPicture($projectId, $user['preview'], true);
+                $projectName = str_replace('_', ' ', $value['projectname']);
+                $shareLink = SHARE_LINK_ROUTE . $value['sharelink'];
+                $customerName = $value['customername'];
+                $descInfo = $value['extra'];
+                $startDate = $value['date_in'];
+                $revision = $value['revision'];
+                ?>
 
-                <div class="card mb-4 shadow-sm">
-                    <!--  Project Name and Share Link -->
-                    <h5 class="card-title position-relative"><b class="text-primary">Name:</b> <?= $projectName; ?>
-                        <span class="text-primary share-project position-absolute end-0 me-3" data-share-link="<?= $shareLink; ?>">
+                <div class="col-md-4">
+
+                    <div class="card mb-4 shadow-sm">
+                        <!--  Project Name and Share Link -->
+                        <h5 class="card-title position-relative"><b class="text-primary">Name:</b> <?= $projectName; ?>
+                            <span class="text-primary share-project position-absolute end-0 me-3" data-share-link="<?= $shareLink; ?>">
 					<i class="bi bi-share-fill"></i>
 				</span>
-                    </h5>
+                        </h5>
 
-                    <?php
-                    //Project Documentation preview or Last step of project if Docs not exist
-                    if ($user['preview'] == 'docs') {
-                        if (!empty($value['projectdocs']) && strpos($value['projectdocs'], '.pdf') !== false) { ?>
-                            <iframe src="<?= $value['projectdocs']; ?>"></iframe>
-                            <a href="<?= $value['projectdocs']; ?>" target="_blank" class="mt-2 pdf-link">View Project Docs</a>
-                        <?php } else { ?>
-                            <img src="<?= $imgPath; ?>" alt="<?= $projectName; ?>" class="img-fluid">
-                        <?php }
-                    } ?>
-                    <!-- photo gallery for project -->
-                    <div class="photo-gallery">
                         <?php
-                        if ($user['preview'] == 'image' && $imgPath) {
-                            $firstImg = reset($imgPath); // Сброс указателя массива и получение первого элемента
-                            foreach ($imgPath as $img) {
-                                $display = ($img === $firstImg) ? '' : 'hidden';
-                                ?>
-                                <img src="<?= $img['image']; ?>" alt="<?= $projectName; ?>" class="img-fluid gallery-photo <?= $display; ?>">
-                                <?php
+                        //Project Documentation preview or Last step of project if Docs not exist
+                        if ($user['preview'] == 'docs') {
+                            if (!empty($value['projectdocs']) && strpos($value['projectdocs'], '.pdf') !== false) { ?>
+                                <iframe src="<?= $value['projectdocs']; ?>"></iframe>
+                                <a href="<?= $value['projectdocs']; ?>" target="_blank" class="mt-2 pdf-link">View Project Docs</a>
+                            <?php } else { ?>
+                                <img src="<?= $imgPath; ?>" alt="<?= $projectName; ?>" class="img-fluid">
+                            <?php }
+                        } ?>
+                        <!-- photo gallery for project -->
+                        <div class="photo-gallery">
+                            <?php
+                            if ($user['preview'] == 'image' && $imgPath) {
+                                $firstImg = reset($imgPath); // Сброс указателя массива и получение первого элемента
+                                foreach ($imgPath as $img) {
+                                    $display = ($img === $firstImg) ? '' : 'hidden';
+                                    ?>
+                                    <img src="<?= $img['image']; ?>" alt="<?= $projectName; ?>" class="img-fluid gallery-photo <?= $display; ?>">
+                                    <?php
+                                }
                             }
-                        }
-                        ?>
-                    </div>
+                            ?>
+                        </div>
 
-                    <div class="card-body">
-                        <!--  Customer or Company Name -->
-                        <p class="card-text"><b class="text-primary">Customer:</b><br/> <?= $customerName; ?></p>
+                        <div class="card-body">
+                            <!--  Customer or Company Name -->
+                            <p class="card-text"><b class="text-primary">Customer:</b><br/> <?= $customerName; ?></p>
 
-                        <!-- Some additional information about project, view if exist -->
-                        <?php if (!empty($descInfo)) { ?>
-                            <p class="card-text"><b class="text-primary">Additional Info:</b><br/><?= $descInfo; ?></p>
-                        <?php } ?>
+                            <!-- Some additional information about project, view if exist -->
+                            <?php if (!empty($descInfo)) { ?>
+                                <p class="card-text"><b class="text-primary">Additional Info:</b><br/><?= $descInfo; ?></p>
+                            <?php } ?>
 
-                        <!-- project created or updated Date and Revision -->
-                        <p class="card-text text-primary"><?= $startDate; ?> &nbsp; <small class="text-danger">Rev:&nbsp;<?= $revision; ?></small></p>
+                            <!-- project created or updated Date and Revision -->
+                            <p class="card-text text-primary"><?= $startDate; ?> &nbsp; <small class="text-danger">Rev:&nbsp;<?= $revision; ?></small></p>
 
-                        <!--  action buttons group  -->
-                        <div class="btn-group">
-                            <a type="button" title="Preview" class="btn btn-outline-info anchor-btn" href="/edit_project?pid=<?= $projectId; ?>">
-                                <i class="bi bi-eye"></i>
-                            </a>
+                            <!--  action buttons group  -->
+                            <div class="btn-group">
+                                <a type="button" title="Preview" class="btn btn-outline-info anchor-btn" href="/edit_project?pid=<?= $projectId; ?>">
+                                    <i class="bi bi-eye"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <?php
+                <?php
+            }
         }
     }
 }
